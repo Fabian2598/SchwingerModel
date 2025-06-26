08e1d33e4b1cc9b65d300b397b1a2fc7aaab7913
nietocastellanos1 <j.nieto.castellanos@fz-juelich.de>
2025-06-26 17:19:24 +0200

    open conf file

--- AMG/src/main.cpp
+++ AMG/src/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
     initialize_matrices(); //Initialize gamma matrices, identity and unit vectors
     Coordinates(); //Builds array with coordinates of the lattice points x * Nt + t 
     periodic_boundary(); //Builds LeftPB and RightPB (periodic boundary for U_mu(n))
-    double m0 = -0.75;
+    double m0 = -0.19494584837545137; 
 
     //Default values in variables.cpp
     sap_gmres_restart_length = 20; //GMRES restart length for the Schwarz blocks. Set to 20 by default
@@ -106,6 +106,33 @@ int main(int argc, char **argv) {
     
     GaugeConf GConf = GaugeConf(Nx, Nt);
     GConf.initialize(); //Initialize a random gauge configuration
+
+    //Open conf from file//
+    /*
+    {
+        //Open Conf File//
+        char NameData[500];
+        //sprintf(NameData, "../confs/2D_U1_Ns%d_Nt%d_b%s_m%s_%d.txt", Ns, Nt, format(beta).c_str(), format(m0).c_str(), n);
+        sprintf(NameData, "L016x016_b02000_k27700_%d.ctxt",700);
+        std::ifstream infile(NameData);
+        if (!infile) {
+            std::cerr << "File not found on rank " << rank << std::endl;
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        int x, t, mu;
+        double re, im;
+        
+        c_matrix CONF(Ntot,c_vector(2,0)); 
+        while (infile >> x >> t >> mu >> re >> im) {
+            //fortran indexing
+            CONF[Coords[x-1][t-1]][mu] = c_double(re, im); 
+        }
+        GConf.setGconf(CONF);
+        infile.close();
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    */
+
 
     spinor rhs(Ntot, c_vector(2, 0)); //random right hand side 
     spinor x(Ntot, c_vector(2, 0)); //solution vector 
