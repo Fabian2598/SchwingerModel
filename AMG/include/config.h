47e7573519ef9871b61d278f730bebd07508dc1d
nietocastellanos1 <j.nieto.castellanos@fz-juelich.de>
2025-09-23 16:15:28 +0200

    Two level tests

--- AMG/include/config.h
+++ AMG/include/config.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-#define NS 64
-#define NT 64
+#define NS 128
+#define NT 128
 #define BLOCK_X 4
 #define BLOCK_T 4
-#define NTEST 10
+#define NTEST 20
 #define SAP_BLOCK_X 4
 #define SAP_BLOCK_T 4
 
