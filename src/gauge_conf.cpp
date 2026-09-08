64aec5e7ce38f14ca0b5ae921f73b8f4c16c595b
nietocastellanos1 <j.nieto.castellanos@fz-juelich.de>
2026-09-08 15:59:48 +0200

    first MPI implementation of Q_mv
    I have to test the term

--- src/gauge_conf.cpp
+++ src/gauge_conf.cpp
@@ -54,6 +54,58 @@ void GaugeConf::Compute_Plaquette01() {
 //mu = 0 time direction, mu = 1 space direction
 void GaugeConf::Compute_Q() {
     //Halo must be communicated externally
+    //Corners we have to communicate manually 
+    //Update top-right corner (needs bottom-left corner from diagonal rank)
+    {
+        int x0 = mpi::width_x, t0 = 1;
+        int n0 = x0*(mpi::width_t+2)+t0;
+        c_double bottom_left_s0 = Conf.val[2*n0];   //U_0(n-1+0)
+        c_double bottom_left_s1 = Conf.val[2*n0+1]; //U_1(n-1+0)
+        MPI_Send(&bottom_left_s0, 1, MPI_DOUBLE_COMPLEX, mpi::bot_left, 0, mpi::cart_comm);
+        MPI_Recv(&bottom_left_s0, 1, MPI_DOUBLE_COMPLEX, mpi::top_right, 0, mpi::cart_comm, &status);
+
+        MPI_Send(&bottom_left_s1, 1, MPI_DOUBLE_COMPLEX, mpi::bot_left, 1, mpi::cart_comm);
+        MPI_Recv(&bottom_left_s1, 1, MPI_DOUBLE_COMPLEX, mpi::top_right, 1, mpi::cart_comm, &status);
+        n0 = mpi::width_t+1; 
+        Conf.val[2*n0] = bottom_left_s0;  
+        Conf.val[2*n0+1] = bottom_left_s1;    
+    }
+
+    //Update bottom-left corner (needs top-right corner from diagonal rank)
+    {
+        int x0 = 1, t0 = mpi::width_t;
+        int n0 = x0*(mpi::width_t+2)+t0;
+        c_double top_right_s0 = Conf.val[2*n0];     //U_0(n+1-0)
+        c_double top_right_s1 = Conf.val[2*n0+1];   //U_0(n+1-0)
+        MPI_Send(&top_right_s0, 1, MPI_DOUBLE_COMPLEX, mpi::top_right, 2, mpi::cart_comm);
+        MPI_Recv(&top_right_s0, 1, MPI_DOUBLE_COMPLEX, mpi::bot_left, 2, mpi::cart_comm, &status);
+
+        MPI_Send(&top_right_s1, 1, MPI_DOUBLE_COMPLEX, mpi::top_right, 3, mpi::cart_comm);
+        MPI_Recv(&top_right_s1, 1, MPI_DOUBLE_COMPLEX, mpi::bot_left, 3, mpi::cart_comm, &status);
+
+        x0 = mpi::width_x+1; t0 = 0;
+        n0 = x0*(mpi::width_t+2)+t0;
+        Conf.val[2*n0] = top_right_s0;
+        Conf.val[2*n0+1] = top_right_s1;
+    }
+    //Update top-left corner (needs bot-right corner from diagonal rank)
+    {
+        int x0 = mpi::width_x, t0 = mpi::width_t;
+        int n0 = x0*(mpi::width_t+2)+t0;
+        c_double bot_right_s0 = Conf.val[2*n0];   //U_0(n-1-0)
+        c_double bot_right_s1 = Conf.val[2*n0+1]; //U_1(n-1-0)
+        MPI_Send(&bot_right_s0, 1, MPI_DOUBLE_COMPLEX, mpi::bot_right, 4, mpi::cart_comm);
+        MPI_Recv(&bot_right_s0, 1, MPI_DOUBLE_COMPLEX, mpi::top_left, 4, mpi::cart_comm, &status);
+
+        MPI_Send(&bot_right_s1, 1, MPI_DOUBLE_COMPLEX, mpi::bot_right, 5, mpi::cart_comm);
+        MPI_Recv(&bot_right_s1, 1, MPI_DOUBLE_COMPLEX, mpi::top_left, 5, mpi::cart_comm, &status);
+
+        n0 = 0;
+        Conf.val[2*n0]   = bot_right_s0;
+        Conf.val[2*n0+1] = bot_right_s1;
+    }
+
+
     int n, right, down, left, up;
     double lsign, rsign;
     int x1_t_1, x_1_t_1, x_1_t1; //n-0+1, n-0-1, n+0-1
